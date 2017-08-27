341b37a8b67eb5e269db739535cfcfcc2018beee
jtough <jtoughgithub@gmail.com>
2017-08-27 17:47:42 -0300

    Updated HOWTO file and changed test message in Application.java

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker World";
+		return "This is my Spring Boot webapp";
 	}
 
 	public static void main(String[] args) {
